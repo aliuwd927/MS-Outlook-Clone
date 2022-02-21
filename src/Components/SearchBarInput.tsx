7b001809b3f4e9df7ce69cb05b1f5a779b3c2292
Andy Liu <aliu927@gmail.com>
2022-02-20 18:30:26 -0800

    trying to figure out logic for search input

--- src/Components/SearchBarInput.tsx
+++ src/Components/SearchBarInput.tsx
@@ -8,14 +8,28 @@ import React, {FC} from 'react';
 }
 
 export const SearchBarInput: FC<SearchProp> = ({value="",placeholder})=>{
-  return (<input 
-          value={value}
-          placeholder={placeholder}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue = event.target.value
-            console.log(newValue);
-          }}
-          >
-          </input>
+  return (
+    <input 
+      value={value}
+      placeholder={placeholder}
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.target.value
+      console.log(newValue);
+      }}
+      >
+      </input>
   )
 }
+
+/*
+
+Question: What will the search bar input do?
+
+It will filter out current map, and return the email we are looking for
+
+we take value from search bar input
+if search is empty, return standard emails
+if search is incorrect show nothing
+if search contains show it ( could possible have multiples emails with certain word )
+
+*/
