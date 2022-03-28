0a1e68ad3cf1d854068dd7ea0339474d06e1703a
Andy Liu <aliu927@gmail.com>
2022-03-28 10:30:01 -0700

    Added Form Groups

--- src/Components/NavContainer.tsx
+++ src/Components/NavContainer.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Dropdown, Navbar, Modal } from "react-bootstrap";
+import {
+  Dropdown,
+  Navbar,
+  Modal,
+  Form,
+  Button,
+  CloseButton,
+} from "react-bootstrap";
 import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
 
 export default function NavContainer() {
@@ -14,14 +21,47 @@ export default function NavContainer() {
   return (
     <Navbar>
       <Dropdown>
-        <Dropdown.Toggle>
-          <i className="fa fa-bars"></i>
-        </Dropdown.Toggle>
+        <div className="nav_Buttons">
+          <Dropdown.Toggle>
+            <i className="fa fa-bars"></i>
+          </Dropdown.Toggle>
+          <Button variant="danger">
+            <CloseButton disabled />
+          </Button>
+        </div>
+
         <DropdownMenu>
           <Dropdown.Item onClick={handleShow}>New Email</Dropdown.Item>
           <Modal show={show} onHide={handleClose} centered>
             <Modal.Dialog>
-              <Modal.Header>I am Modal Head</Modal.Header>
+              <Modal.Header>
+                <Modal.Title>Enter Email Stuff</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group>
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={() => {
+                      alert();
+                    }}
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              </Modal.Body>
             </Modal.Dialog>
           </Modal>
         </DropdownMenu>
