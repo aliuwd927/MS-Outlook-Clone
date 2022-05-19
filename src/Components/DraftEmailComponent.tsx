377f3605fedce55f1147f211759b6cf0202abe44
Andy Liu <aliu927@gmail.com>
2022-05-18 23:51:27 -0700

    conditonal render / react fragment

--- src/Components/DraftEmailComponent.tsx
+++ src/Components/DraftEmailComponent.tsx
@@ -1,35 +1,58 @@
-import { draftStore } from "./zustand";
+import { draftStore, modalStatus } from "./zustand";
 import ModalComponent from "./ModalComponent";
+import React, { useState } from "react";
+import shallow from "zustand/shallow";
 
 //Type '{}' is missing the following properties from type '{ show: boolean; setShow: any; }': show, setShow
 
 export default function DraftEmailComponent() {
   const draftState = draftStore((state) => state.draftStateArray);
-  console.log(draftState);
+
+  const { showModal, setShowModal } = modalStatus(
+    (state) => ({
+      showModal: state.show,
+      setShowModal: state.setShow,
+    }),
+    shallow
+  );
+
+  const [showDraftModal, setShowDraftModal] = useState(false);
+
+  function isDraftModal() {
+    setShowDraftModal(!showDraftModal);
+    setShowModal(true);
+  }
 
   return (
-    <div>
-      {draftState.map((element, index) => {
-        console.log(element, index);
-        return (
-          <div>
-            <div>{element.nameOfSender}</div>
-            <div>{element.titleOfEmail}</div>
-            <div>{element.bodyMessage}</div>
-          </div>
-        );
-      })}
-    </div>
+    <React.Fragment>
+      {showDraftModal && (
+        <ModalComponent show={showModal} setShow={setShowModal} />
+      )}
+
+      <div>
+        {draftState.map((element) => {
+          return (
+            <div onClick={isDraftModal}>
+              <div>{element.nameOfSender}</div>
+              <div>{element.titleOfEmail}</div>
+              <div>{element.bodyMessage}</div>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 }
 /*
-When we click on it
-Modal pops open
-Fill out presaved items
+Change modal local state to global --- Done.
+When we click on it -- Done.
+Modal pops open --- Done.
+ <div onClick={()=> <ModalComponent show={showModal} setShow={setShowModal} /> }>
+
+Fill out presaved items 
 3 buttons Save Send Close
 When we hit save we update that particular element 
 When we hit send we delete from stack, and send it to sendEmailComponent
 When we hit close, nothing saves / happens,modal just closes
-
 
 */
